72d24f8f32d7ad48855b5677f85f7ee6ef885b88
vladimirsvsv77 <vladimirsvsv77@gmail.com>
2018-08-03 20:48:46 +0300

    added files

---
+++ test_asr.py
@@ -0,0 +1,35 @@
+import io
+import os
+
+from google.cloud import speech
+from google.cloud.speech import enums
+from google.cloud.speech import types
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "your_path"
+
+
+def transcribe_file(speech_file):
+    """Transcribe the given audio file."""
+    client = speech.SpeechClient()
+
+    with io.open(speech_file, 'rb') as audio_file:
+        content = audio_file.read()
+
+    audio = types.RecognitionAudio(content=content)
+    config = types.RecognitionConfig(
+        encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=16000,
+        language_code='ru-RU')
+    # [END migration_audio_config_file]
+
+    # [START migration_sync_response]
+    response = client.recognize(config, audio)
+    # [END migration_sync_request]
+    # Each result is for a consecutive portion of the audio. Iterate through
+    # them to get the transcripts for the entire audio file.
+    for result in response.results:
+        # The first alternative is the most likely one for this portion.
+        print(u'Transcript: {}'.format(result.alternatives[0].transcript))
+
+
+transcribe_file('file.wav')
